7cccfaf371622110d7a27325423fa0790d09bdf4
miykael <michaelnotter@hotmail.com>
2018-07-19 00:51:07 +0200

    ENH: update and restructure fragmenter

--- fragmenter.py
+++ fragmenter.py
@@ -2,20 +2,11 @@ import numpy as np
 from nibabel import freesurfer
 from nilearn.plotting import plot_surf_roi
 from sklearn import cluster, mixture, neighbors
-from time import time
-
-"""
-# Notes:
- - Can perhaps be optimized with something like
-   https://github.com/NeuroanatomyAndConnectivity/surfdist/
- - perhaps better with dijkstra algorithm, but computation takes forever
- - perhaps better with something like `import gdist` - but it all takes way too long
-"""
 
 
-def get_spaced_colors(n):
+def get_equally_spaced_colors(n):
     """
-    Returns a colortable with N elements, equally spaced throughout RGB-space
+    Returns a color table with N elements, equally spaced throughout RGB-space
     """
 
     max_value = 256**3 - 1
@@ -28,231 +19,173 @@ def get_spaced_colors(n):
     return color_table
 
 
-def get_equally_spaced_points(coords, num_pts=100):
-    """Return indices of vertices that cover the spherical mesh equally"""
+def get_ctab_and_names(n_parcel, coords, labels, use_pretty_colors=True):
+    """
+    Returns color table and names - pretty color gradient order optional
+    """
 
-    indices = np.arange(0, num_pts, dtype=float) + 0.5
+    # Create color table (first element = [0, 0, 0] for 'unknown' region)
+    n_colors = n_parcel + 1
+    ctab = np.hstack((get_equally_spaced_colors(n_colors), [[0]] * n_colors))
 
-    phi = np.arccos(1 - 2 * indices / num_pts)
-    theta = np.pi * (1 + 5**0.5) * indices
+    # Create name list for new regions
+    names = [np.bytes_('unknown')] + \
+        [np.bytes_('parc%05d' % (i + 1)) for i in range(n_parcel)]
 
-    # Compute location of equally spaced points on a sphere
-    x = np.cos(theta) * np.sin(phi)
-    y = np.sin(theta) * np.sin(phi)
-    z = np.cos(phi)
+    # Reorder table and names according height on z-axis if requested
+    if use_pretty_colors:
 
-    # Compute sphere radius
-    sphere_radius = np.mean(np.linalg.norm(coords, axis=1))
+        # Compute mean height (on z-axis) per label to resort color table
+        label_centers = np.array(
+            [np.mean(coords[labels == l], axis=0) for l in np.unique(labels)])
+        label_order = np.argsort(label_centers[:, 2])
 
-    # Adjust equally spaced points to requested sphere radius
-    equal_points = np.vstack((x, y, z)).T * sphere_radius
+        # Verify that no label is missing from the data
+        assert len(np.unique(label_order)) == n_parcel
 
-    return np.array(equal_points)
-
-
-def get_ctab_and_names(parcel_N, coords):
-
-    # Create color table
-    color_N = parcel_N + 1  # one more to keep 0 0 0 for color 'unknown'
-    ctab = np.hstack((get_spaced_colors(color_N), [[0]] * color_N))
-
-    # Create name list for parcellations
-    names = [np.bytes_('unknown')] + [np.bytes_('parc%04d' % (i + 1))
-                                      for i in range(parcel_N)]
-
-    # Get center points
-    center_points = get_equally_spaced_points(coords, num_pts=parcel_N)
-
-    return ctab, names, center_points
-
-
-def reorder_ctab_and_names(coords, labels, parcel_N, ctab, names):
-
-    # Compute mean height (on z-axis) per label to resort color table
-    label_centers = np.array(
-        [np.mean(coords[labels == l], axis=0) for l in np.unique(labels)])
-    label_order = np.argsort(label_centers[:, 2])
-
-    # Verify that no label is missing from the data
-    assert len(np.unique(label_order)) == parcel_N
-
-    # Create the reordered color table
-    ctab = np.vstack((ctab[0], ctab[1:][label_order]))
-    names = [names[0]] + np.array(names)[1:][label_order].tolist()
+        # Reorder color table and region names
+        ctab = np.vstack((ctab[0], ctab[1:][label_order]))
+        names = [names[0]] + np.array(names)[1:][label_order].tolist()
 
     return ctab, names
 
 
-def parcel_spherically(coords, parcel_N):
+def fragment_parcel(coords, n_parcel, algorithm='kmeans', mask=None,
+                    use_pretty_colors=True, output_file='_h.fragment.annot'):
+    """
+    Fragments specified region according to a given clustering algorithm
+    """
 
-    ctab, names, center_points = get_ctab_and_names(parcel_N, coords)
+    # Mask coordinates with roi_mask
+    roi_coords = coords[mask]
 
-    # Compute distance to center points
-    center_distances = np.linalg.norm(
-        np.array([coords - c for c in center_points]), axis=2)
+    if algorithm == 'kmeans':
 
-    # Compute the labels for each vertex
-    labels = np.argmin(center_distances, axis=0) + 1
+        # Run Mini Batch KMeans algorithm
+        k_means = cluster.MiniBatchKMeans(
+            n_clusters=n_parcel, init='k-means++', max_iter=1000,
+            batch_size=10000, verbose=True, compute_labels=True,
+            max_no_improvement=100, n_init=5, reassignment_ratio=0.1)
+        k_means.fit(roi_coords)
 
-    ctab, names = reorder_ctab_and_names(coords, labels, parcel_N, ctab, names)
+        # Extract labels for each vertex and increase values by 1
+        labels = np.copy(k_means.labels_).astype(np.int) + 1
 
-    out_filename = 'freesurfer/fsaverage/label/lh.parc_sphere_%04d.annot' % parcel_N
+    elif algorithm == 'gmm':
 
-    freesurfer.write_annot(out_filename, labels, ctab, names, fill_ctab=True)
+        # Run Gaussian Mixture Model algorithm
+        gmm = mixture.GaussianMixture(
+            n_components=n_parcel, covariance_type='tied', max_iter=1000,
+            init_params='kmeans', verbose=1)
+        gmm.fit(roi_coords)
 
-    return out_filename
+        # Extract labels for each vertex and increase values by 1
+        labels = np.copy(gmm.predict(roi_coords)).astype(np.int) + 1
 
+    elif algorithm == 'ward':
 
-def parcel_KMeans(coords, parcel_N):
+        # Run AgglomerativeClustering and Ward algorithm
+        knn_graph = neighbors.kneighbors_graph(
+            roi_coords, n_neighbors=20, mode='connectivity',
+            metric='minkowski', p=2, include_self=False, n_jobs=-1)
 
-    ctab, names, center_points = get_ctab_and_names(parcel_N, coords)
+        model = cluster.AgglomerativeClustering(
+            n_clusters=n_parcel, affinity='euclidean', connectivity=knn_graph,
+            linkage='ward')
 
-    # Run Mini Batch KMeans algorithm
-    k_means = cluster.MiniBatchKMeans(
-        n_clusters=parcel_N, max_iter=1000, batch_size=50000,
-        init=center_points, n_init=1, reassignment_ratio=0.1)
-    k_means.fit(coords)
+        model.fit(roi_coords)
 
-    # Compute the labels for each vertex
-    labels = k_means.labels_.astype(np.int) + 1
+        # Extract labels for each vertex and increase values by 1
+        labels = np.copy(model.labels_).astype(np.int) + 1
 
-    ctab, names = reorder_ctab_and_names(coords, labels, parcel_N, ctab, names)
+    # Create color table and region names for annotation file
+    ctab, names = get_ctab_and_names(
+        n_parcel, roi_coords, labels, use_pretty_colors)
 
-    out_filename = 'freesurfer/fsaverage/label/lh.parc_kmeans_%04d.annot' % parcel_N
+    # Extend labels to whole spherical surface mesh
+    surface_labels = np.zeros(mask.shape, dtype='int')
+    surface_labels[mask] = labels
 
-    freesurfer.write_annot(out_filename, labels, ctab, names, fill_ctab=True)
+    # Create annotation file
+    freesurfer.write_annot(
+        output_file, surface_labels, ctab, names, fill_ctab=True)
 
-    return out_filename
-
-
-def parcel_GMM(coords, parcel_N):
-
-    ctab, names, center_points = get_ctab_and_names(parcel_N, coords)
-
-    # Run Gaussian Mixture algorithm
-    gmm = mixture.GaussianMixture(
-        n_components=parcel_N, covariance_type='tied')
-    gmm.fit(coords)
-
-    # Compute the labels for each vertex
-    labels = gmm.predict(coords).astype(np.int) + 1
-
-    ctab, names = reorder_ctab_and_names(coords, labels, parcel_N, ctab, names)
-
-    out_filename = 'freesurfer/fsaverage/label/lh.parc_gmm_%04d.annot' % parcel_N
-
-    freesurfer.write_annot(out_filename, labels, ctab, names, fill_ctab=True)
-
-    return out_filename
-
-
-def parcel_Ward(coords, parcel_N):
-
-    ctab, names, center_points = get_ctab_and_names(parcel_N, coords)
-
-    # AgglomerativeClustering with ward
-    knn_graph = neighbors.kneighbors_graph(
-        coords, 50, include_self=False, n_jobs=-1,
-        mode='connectivity', metric='minkowski', p=2)
-
-    model = cluster.AgglomerativeClustering(
-        linkage='ward', connectivity=knn_graph, n_clusters=parcel_N)
-
-    model.fit(coords)
-
-    labels = model.labels_.astype(np.int) + 1
-
-    ctab, names = reorder_ctab_and_names(coords, labels, parcel_N, ctab, names)
-
-    out_filename = 'freesurfer/fsaverage/label/lh.parc_ward_%04d.annot' % parcel_N
-
-    freesurfer.write_annot(out_filename, labels, ctab, names, fill_ctab=True)
-
-    return out_filename
+    return output_file
 
 
-def plot_parcellation(parc_filename, out_filename, surface):
+def get_roi_mask(roi_idx, fpath_annot):
+    """
+    Creates a region mask specifying which vertexes to consider
+    """
 
-    parcellation = freesurfer.read_annot(parc_filename)
+    # Load annotation file
+    roi_labels, _, roi_names = freesurfer.read_annot(fpath_annot)
 
-    surf = 'freesurfer/fsaverage/surf/lh.%s' % surface
+    # Get index of rois to consider
+    roi_index = [roi_names.index(np.bytes_(n)) for n in roi_idx]
 
+    # Create vertex mask
+    roi_mask = np.isin(roi_labels, np.array(roi_index))
+
+    # Take all regions if none specified
+    if roi_mask.sum() == 0:
+        roi_mask = np.invert(roi_mask)
+
+    return roi_mask
+
+
+def plot_fragment(frag_file, filename, fpath_sphere, surface):
+    """
+    Plot fragmented region on specific surface
+    """
+
+    # Load labels of new fragmented annotation
+    labels, _, _ = freesurfer.read_annot(frag_file)
+
+    # Specify on which surface to plot
+    surf_type = fpath_sphere.replace('.sphere', '.%s' % surface)
+
+    # Plot fragmented region on surface with colored annotation
     plot_surf_roi(
-        surf, roi_map=parcellation[0], hemi='left', view='lateral',
-        cmap='Spectral', output_file=out_filename)
+        surf_type, roi_map=labels, hemi='left', view='lateral',
+        cmap='Spectral', output_file=filename)
 
 
 if __name__ == '__main__':
 
-    filename = 'freesurfer/fsaverage/surf/lh.sphere'
+    # Specify name of subject and hemisphere to work on
+    sub_id = 'fsaverage'
+    hemi = 'lh'
 
-    coords, _ = freesurfer.read_geometry(filename)
+    # Specify location of subject specific sphere mesh and annotation file
+    fpath_sphere = 'freesurfer/%s/surf/%s.sphere' % (sub_id, hemi)
+    fpath_annot = 'freesurfer/%s/label/%s.aparc.annot' % (sub_id, hemi)
 
-    """
-    for parcel_N in [2**4, 2**6, 2**8, 2**10, 2**12]:
+    # Load vertex coordinates
+    coords, _ = freesurfer.read_geometry(fpath_sphere)
 
-        t = time()
-        parc_file_spherically = parcel_spherically(coords, parcel_N)
-        print('spherically\t%04d\t%.03f' % (parcel_N, time() - t))
+    # Specify number of fragments requested in annotation region
+    n_parcel = 2**4
 
-    for parcel_N in [2**4, 2**6, 2**8, 2**10, 2**12]:
+    # Specify name of regions to consider (empty = all regions)
+    roi_idx = ['superiortemporal', 'transversetemporal']
+    roi_mask = get_roi_mask(roi_idx, fpath_annot)
 
-        t = time()
-        parc_file_KMeans = parcel_KMeans(coords, parcel_N)
-        print('KMeans\t\t%04d\t%.03f' % (parcel_N, time() - t))
+    # Create fragmented annotation file
+    algorithm = 'kmeans'
+    fpath_out = 'freesurfer/%s/label/lh.%s.annot' % (
+        sub_id, '%s_%05d' % (algorithm, n_parcel))
 
-        for surface in ['inflated', 'sphere', 'pial']:
-                plot_parcellation(
-                    parc_file_KMeans, 'plot_kmeans_%s_%04d.png' % (surface, parcel_N), surface)
+    fragment_file = fragment_parcel(
+        coords, n_parcel, algorithm=algorithm, mask=roi_mask,
+        use_pretty_colors=True, output_file=fpath_out)
 
-    for parcel_N in [2**4, 2**6, 2**8, 2**10, 2**12]:
-
-        t = time()
-        parc_file_GMM = parcel_GMM(coords, parcel_N)
-        print('GMM\t\t%04d\t%.03f' % (parcel_N, time() - t))
-
-        for surface in ['inflated', 'sphere', 'pial']:
-                plot_parcellation(
-                    parc_file_GMM, 'plot_gmm_%s_%04d.png' % (surface, parcel_N), surface)
-
-    """
-
-    for parcel_N in [2**4, 2**6, 2**8, 2**10, 2**12]:
-
-        t = time()
-        parc_file_Ward = parcel_Ward(coords, parcel_N)
-        print('Ward\t\t%04d\t%.03f' % (parcel_N, time() - t))
-
-        for surface in ['inflated', 'sphere', 'pial']:
-
-                plot_parcellation(
-                    parc_file_Ward, 'plot_ward_%s_%04d.png' % (surface, parcel_N), surface)
-
-
-    # For validation
-    # mri_segstats --annot fsaverage lh parc_Ward --sum summary_file.txt
-
-
-"""
-spherically 0016     0.238
-spherically 0016     0.224
-spherically 0064     0.736
-spherically 0256     2.648
-spherically 1024    10.304
-spherically 4096    49.146
-KMeans      0016     0.919
-KMeans      0064     1.314
-KMeans      0256     4.232
-KMeans      1024    19.433
-KMeans      4096    91.256
-GMM         0016     4.755
-GMM         0064    25.177
-GMM         0256    78.734
-GMM         1024   241.126
-GMM         4096   730.494
-Ward        0016    82.463
-Ward        0064
-Ward        0256
-Ward        1024
-Ward        4096
-"""
+    # Plot fragmented parcellation
+    surfaces_to_plot = ['inflated', 'sphere', 'pial']
+    surfaces_to_plot = ['sphere']
+    for surface in surfaces_to_plot:
+        plot_fragment(fragment_file,
+                      'plot_%s_%s_%05d.png' % (
+                          algorithm, surface, n_parcel),
+                      fpath_sphere,
+                      surface)
