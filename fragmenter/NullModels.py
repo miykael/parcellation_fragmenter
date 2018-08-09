757bce9642ab1c2f2501960ab5f8acadcd5c1e9e
Kristian Eschenburg <keschenb@uw.edu>
2018-08-08 22:50:14 -0700

    test copy

---
+++ fragmenter/NullModels.py
@@ -0,0 +1,66 @@
+from fragmenter import rotations
+import numpy as np
+from scipy.spatial import KDTree
+
+
+class NullModel(object):
+    """
+    Class to generate null models of parcellations by perturbing an existing
+    map using KD-Trees for the nearest neighor search.
+
+    Parameters:
+    - - - - -
+        sphere: (N,3) array
+            vertex coordinates of a spherical mesh
+        label: (N,1)) array
+            vertex labels
+    """
+
+    def __init__(self, sphere, label, mask=None):
+
+        self.label = label
+        self.sphere = sphere
+        self.mask = mask
+
+    def fit(self):
+        """
+        Wrapper method for generating null models.
+        """
+
+        rotated = self._rotate()
+
+        if not np.any(self.mask):
+                mask = np.arange(len(self.labels))
+
+        print('Fitting KD-Tree')
+        K = KDTree(rotated)
+
+        print('Querying tree for nearest neighbors.')
+        kdnn = K.query(self.sphere[mask, :], k=1)
+        kd_label = self.label[kdnn[1]]
+
+        return kd_label
+
+    def _rotate(self, maxd_x=10, maxd_y=10, maxd_z=10):
+        """
+        Randomly rotate the original spherical mesh.
+
+        Parameters:
+        - - - - -
+            maxd_x, maxd_y, maxd_z : float
+                maximum range of angle to sample for each direction
+        """
+
+        x = np.deg2rad(np.random.uniform(-maxd_x, maxd_x, 1))
+        y = np.deg2rad(np.random.uniform(-maxd_y, maxd_y, 1))
+        z = np.deg2rad(np.random.uniform(-maxd_z, maxd_z, 1))
+
+        rx = rotations.rotx(x)
+        ry = rotations.roty(y)
+        rz = rotations.rotz(z)
+
+        composed = rx.dot(ry.dot(rz))
+
+        rotated = self.sphere.dot(composed)
+
+        return rotated
