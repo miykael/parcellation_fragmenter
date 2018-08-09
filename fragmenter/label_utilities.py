57b0c2b8d1d8468229112d14d0255c425f759019
Kristian Eschenburg <keschenb@uw.edu>
2018-08-08 23:13:39 -0700

    copy script files to fragmenter

---
+++ fragmenter/label_utilities.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def region_indices(lab_obj, regions=None):
+
+    cdata = lab_obj.darrays[0].data
+    lt = lab_obj.get_labeltable().get_labels_as_dict()
+    reg2lab = dict(zip(map(str, lt.values()), lt.keys()))
+
+    if not regions:
+        indices = np.arange(len(cdata))
+    else:
+        indices = []
+
+        for r in regions:
+            indices.append(np.where(cdata == reg2lab[r])[0])
+        indices = np.concatenate(indices)
+
+    return indices
